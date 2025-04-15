54edb7b8816a60dfba5b7f1646b4e259b7a0940a
Tom Okafor <tmeka008@gmail.com>
2025-04-15 14:12:29 +0100

    feat: update sections on window resize

--- src/menu/components/menu-block/index.jsx
+++ src/menu/components/menu-block/index.jsx
@@ -1,45 +1,22 @@
 import { MenuList } from "../menu-list";
 import MenuPageHeader from "../menuPageHeader";
 import { CurrentSectionContext } from "../../context";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { setCurrentMenuListSection } from "../../utils/menuListScrollHandler";
 export default function MenuBlock() {
   const [currentSection, setCurrentSection] = useState(0);
   const scrollHandler = () => {
-    const scrollContainer = document.getElementById("menu-block");    
-    const menuCategoryList = Array.from(
-      document.querySelectorAll(".menu-category")
-    );
-    const categoryListTopPostition = menuCategoryList.map((category) =>
-      Math.ceil(
-        category.getBoundingClientRect().top + scrollContainer.scrollTop
-      ) - 200
-    );
-
-    if (currentSection < categoryListTopPostition.length - 1)
-      handleSectionChangeOnScrollUp();
-    if (currentSection > 0) handleSectionChangeOnScrollDown();
-    function handleSectionChangeOnScrollUp() {
-      if (
-        categoryListTopPostition[currentSection + 1] <= scrollContainer.scrollTop
-      ) {
-        console.log(typeof categoryListTopPostition[currentSection + 1]);
-        
-        
-        setCurrentSection((prev) => ++prev);
-        console.log("scrolling up");
-        console.log(menuCategoryList[currentSection + 1]);
-      }
-    }
-    function handleSectionChangeOnScrollDown() {
-      if (
-        categoryListTopPostition[currentSection] > scrollContainer.scrollTop
-      ) {
-        setCurrentSection((prev) => --prev);
-        console.log("scrolling down");
-        console.log(menuCategoryList[currentSection - 1]);
-      }
-    }
+    const newSection = setCurrentMenuListSection(currentSection);
+    newSection !== currentSection && setCurrentSection(newSection);
   };
+  useEffect(() => {
+    window.addEventListener("resize", scrollHandler);
+
+    return () => {
+      window.removeEventListener("resize", scrollHandler);
+    };
+  });
+
   return (
     <section
       className="w-full h-screen overflow-auto scroll-smooth"
