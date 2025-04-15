fafd9dc30daf5a52d9e97d7c6d695fb1382ca936
Tom Okafor <tmeka008@gmail.com>
2025-04-15 19:06:05 +0100

    style: add interactivity to form button

--- src/common/components/form-button.jsx
+++ src/common/components/form-button.jsx
@@ -1,6 +1,6 @@
 export default function FormButton({ text }) {
   return (
-    <button className="w-full bg-primary-bg rounded-full h-[clamp(45px,6vh,60px)] text-inverse text-default-size uppercase font-Chillax-medium flex items-center justify-center">
+    <button className="w-full bg-primary-bg rounded-full h-[clamp(45px,6vh,60px)] text-inverse text-default-size uppercase font-Chillax-medium flex items-center justify-center cursor-pointer hover:bg-primary duration-300 hover:saturate-200">
       {text}
     </button>
   );
