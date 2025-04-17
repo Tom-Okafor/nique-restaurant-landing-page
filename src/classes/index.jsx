a3c030a019e62bd8f364c8fce920866a90f43896
Tom Okafor <tmeka008@gmail.com>
2025-04-17 20:13:54 +0100

    refactor: remove pointer text when hovered text element to improve readability, style: add mix-blend-mode to pointer for a more pleasing UI effect

--- src/classes/index.jsx
+++ src/classes/index.jsx
@@ -1,16 +1,46 @@
+import { useState } from "react";
 import Logo from "../common/components/logo";
 import Menu from "../common/components/nav.jsx";
 import { classBlocks } from "./constants/index.js";
 import { NavLink } from "react-router-dom";
-import clsx from "clsx";
 
 export default function Classes() {
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+  const [isMouseOverText, setIsMouseOverText] = useState(false);
+
+  const { x, y } = coordinates;
+
+  const handlePointerMove = (event) => {
+    const pointerSize = 100;
+    const x = event.clientX - pointerSize / 2;
+    const y = event.clientY - pointerSize / 2;
+    setCoordinates({ x, y });
+  };
+
+  const handleMouseMove = (event) => {
+
+    if (
+      event.target.tagName === "H2" ||
+      event.target.tagName === "P" ||
+      event.target.tagName === "SPAN"
+    ) {
+      setIsMouseOverText(true);
+    } else {
+      setIsMouseOverText(false);
+    }
+  };
   return (
-    <main className="min-h-dvh h-[400vw]  bg-default-bg">
-      <section className="w-full flex flex-col items-center justify-between h-dvh py-16 fixed top-0 z-20">
+    <main
+      className="min-h-dvh h-[400vw]  bg-default-bg"
+      onPointerMove={handlePointerMove}
+      onMouseOver={handleMouseMove}
+    >
+      <div className="fixed top-16 z-20 left-[50vw] translate-x-[-50%]">
         <Logo />
+      </div>
+      <div className="fixed bottom-16 z-20 left-[50vw] translate-x-[-50%]">
         <Menu />
-      </section>
+      </div>
       <section className="flex h-dvh fixed classes-slider z-10">
         {classBlocks.map((block) => (
           <NavLink
@@ -31,6 +61,12 @@ export default function Classes() {
           </NavLink>
         ))}
       </section>
+      <div
+        className="size-[100px] flex justify-center items-center bg-primary-bg text-sm text-inverse font-Chillax-medium rounded-full uppercase fixed z-10 pointer-events-none duration-200 mix-blend-difference"
+        style={{ top: `${y}px`, left: `${x}px` }}
+      >
+        {isMouseOverText ? "" : "explore"}
+      </div>
     </main>
   );
 }
