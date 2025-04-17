3fc79a51126e3b2289b1f4c00a152370a1392218
Tom Okafor <tmeka008@gmail.com>
2025-04-17 15:56:29 +0100

    feat(classes page): add logo and navigation to classes page with initial styling

--- src/classes/index.jsx
+++ src/classes/index.jsx
@@ -1,3 +1,13 @@
+import Logo from "../common/components/logo";
+import Menu from "../common/components/nav.jsx";
+
 export default function Classes() {
-  return <h1 className="text-heading-xlarge font-Chillax-bold">Classes Page</h1>;
+  return (
+    <main className="min-h-dvh h-[400vw] relative bg-default-bg flex justify-center">
+      <section className="flex flex-col items-center justify-between h-dvh py-16 fixed top-0">
+        <Logo />
+        <Menu />
+      </section>
+    </main>
+  );
 }
