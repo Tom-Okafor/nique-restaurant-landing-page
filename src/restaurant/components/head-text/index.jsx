7d85578d47087160de45eeb0c3673f9417f40db5
Tom Okafor <tmeka008@gmail.com>
2025-04-17 02:36:23 +0100

    style(restaurant) animate restaurant head texts on scroll, implementing timing and choreography

--- src/restaurant/components/head-text/index.jsx
+++ src/restaurant/components/head-text/index.jsx
@@ -5,8 +5,8 @@ export default function HeadText() {
   return (
     <section className="w-full h-screen flex flex-col items-center justify-between py-16 z-20 fixed top-0">
       <Logo />
-      <div className="text-center">
-        <div className="hidden">
+      <div className="text-center relative">
+        <div className="text-block-one">
           <p className="text-heading-medium-italic text-primary font-bitter-rose font-[500] capitalize animate-shrink-text-no-delay">
             discover
           </p>
@@ -14,15 +14,15 @@ export default function HeadText() {
             nique.
           </h1>
         </div>
-        <div className="hidden">
-          <p className="text-heading-medium-italic text-primary font-bitter-rose font-[500] capitalize animate-shrink-text-no-delay">
+        <div className="absolute top-[50%] translate-y-[-50%] text-block-two">
+          <p className="text-heading-medium-italic text-primary font-bitter-rose font-[500] animate-shrink-text">
             experience delight
           </p>
           <h1 className="text-heading-xlarge text-default font-Chillax-medium animate-scale-text">
             nique.
           </h1>
         </div>
-        <div>
+        <div className="absolute top-[50%] translate-y-[-50%] text-block-three">
           <p className="text-heading-medium-italic text-primary font-bitter-rose font-[500] animate-shrink-text-no-delay">
             explore the exquisite
           </p>
--- src/restaurant/index.jsx
+++ src/restaurant/index.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import HeadText from "./components/head-text";
 import { restaurantImages } from "./constants";
 
 export default function Restaurant() {
+  useEffect(() => {
+    console.log(document.querySelector(".check").scrollHeight);
+    const section = document.querySelector(".check");
+
+    section.addEventListener("mouseover", handleScroll);
+    function handleScroll() {
+      console.log(section.scrollTop);
+    }
+    return () => {
+      section.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <section className="w-full min-h-screen overflow-x-hidden relative bg-default-bg h-[600vw]">
+    <section className="w-full min-h-dvh relative bg-default-bg h-[600vw] check">
       <HeadText />
       <ul className="z-10 top-0 h-dvh fixed flex translate-x-[0] restaurant-image-slider">
         {restaurantImages.map((restaurantImage, index) => (
@@ -11,7 +24,7 @@ export default function Restaurant() {
             <img
               src={restaurantImage.src}
               alt={restaurantImage.alt}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover object-bottom"
             />
           </li>
         ))}
