25c1185ed4226d68a2ef803283a1a427196a4a27
Tom Okafor <tmeka008@gmail.com>
2025-04-17 15:45:57 +0100

    chore: create object for classes data

---
+++ src/classes/constants/index.js
@@ -0,0 +1,32 @@
+export const classBlocks = [
+  {
+    category: "Asian",
+    title: "Delicious Breakfast",
+    date: "May 9, 2025",
+    image: "classes/delicious-breakfast.png",
+  },
+  {
+    category: "Breakfast",
+    title: "Coffee Time",
+    date: "June 12, 2025",
+    image: "classes/coffee-time.png",
+  },
+  {
+    category: "Vegan",
+    title: "Vegan Burger",
+    date: "July 21, 2025",
+    image: "classes/vegan-burger.png",
+  },
+  {
+    category: "Italian",
+    title: "Salad Style",
+    date: "August 3, 2025",
+    image: "classes/salad-style.png",
+  },
+  {
+    category: "Italian",
+    title: "Homemade Honey",
+    date: "October 15, 2025",
+    image: "classes/homemade-honey.png",
+  },
+];
