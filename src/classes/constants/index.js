9dcbcec8a3c155b7add640d23b83dfa0b227e7e2
Tom Okafor <tmeka008@gmail.com>
2025-04-18 05:15:57 +0100

    refactor: update classes block update to include id

--- src/classes/constants/index.js
+++ src/classes/constants/index.js
@@ -1,29 +1,34 @@
 export const classBlocks = [
   {
+    id: "delicious-breakfast",
     category: "Asian",
     title: "Delicious Breakfast",
     date: "May 9, 2025",
     image: "classes/delicious-breakfast.png",
   },
   {
+    id: "coffee-time",
     category: "Breakfast",
     title: "Coffee Time",
     date: "June 12, 2025",
     image: "classes/coffee-time.png",
   },
   {
+    id: "vegan-burger",
     category: "Vegan",
     title: "Vegan Burger",
     date: "July 21, 2025",
     image: "classes/vegan-burger.png",
   },
   {
+    id: "salad-style",
     category: "Italian",
     title: "Salad Style",
     date: "August 3, 2025",
     image: "classes/salad-style.png",
   },
   {
+    id: "homemade-honey",
     category: "Italian",
     title: "Homemade Honey",
     date: "October 15, 2025",
