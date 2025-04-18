33af2937c34d07973d0fd0d03bce601dbd992b51
Tom Okafor <tmeka008@gmail.com>
2025-04-18 05:51:41 +0100

    feat: add dynamic page title and image section on the class page

--- src/class/index.jsx
+++ src/class/index.jsx
@@ -1,3 +1,20 @@
+import { classBlocks } from "../classes/constants";
+import { useParams } from "react-router-dom";
+import PageTitle from "../common/components/page-title";
+
 export default function Class() {
-  return <h1>Class Page</h1>;
+  const id = useParams().id;
+  const currentClass = classBlocks.find((classBlock) => classBlock.id === id);
+  return (
+    <main className="flex bg-default-bg">
+      <aside>
+        <PageTitle
+          width="50vw"
+          mainText={currentClass.title}
+          subText={currentClass.category}
+          image={`url(/${currentClass.image})`}
+        />
+      </aside>
+    </main>
+  );
 }
