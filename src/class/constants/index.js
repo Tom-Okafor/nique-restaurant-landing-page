769a7572c46267b425836b282190747e0f24ccdf
Tom Okafor <tmeka008@gmail.com>
2025-04-19 17:33:58 +0100

    chore: create class details data and component files for the class main block

---
+++ src/class/constants/index.js
@@ -0,0 +1,9 @@
+export const classDetails = [
+  { caption: "Date", description: ["8:00 AM"] },
+  { caption: "Teacher", description: ["Jakob Grønberg"] },
+  { caption: "Language", description: ["English"] },
+  {
+    caption: "Location",
+    description: ["nique.", "Main Street 16", "10629 Berlin"],
+  },
+];
