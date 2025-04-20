9467cdfa0c4f15bc0875db1e57f79f8a4e533b9d
Tom Okafor <tmeka008@gmail.com>
2025-04-20 23:24:35 +0100

    chore: create data for footer

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -1,2 +1,21 @@
 export const buttonClasses =
   "px-[clamp(14px,0.953vw,16px)] py-[min(16px,1.6vh)] rounded-full bg-default text-inverse text-default-size capitalize font-Chillax-medium hover:bg-primary duration-300 cursor-pointer";
+
+export const footerLinkBlocks = [
+  {
+    title: "pages",
+    blockOne: [
+      "menu",
+      "restaurant",
+      "classes",
+      "book a table",
+      "contact",
+      "blog",
+      "shop",
+    ],
+  },
+  {
+    title: "utility",
+    blockTwo: ["styleguide", "licensing", "404 page"],
+  },
+];
