2e29f709e1ab98c78e6628e3891cda008914f272
Tom Okafor <tmeka008@gmail.com>
2025-04-21 23:55:18 +0100

    feat: create shop list component and merge all sub components to complete the shop page

--- src/shop/components/shop-block/index.jsx
+++ src/shop/components/shop-block/index.jsx
@@ -1,8 +1,10 @@
 import Footer from "../../../common/components/footer";
+import ShopList from "../shop-list";
 
 export default function ShopBlock() {
   return (
     <section className="w-[50vw] flex flex-col gap-12 items-center py-24">
+      <ShopList />
       <Footer />
     </section>
   );
--- src/shop/components/shop-list/index.jsx
+++ src/shop/components/shop-list/index.jsx
@@ -4,14 +4,24 @@ export default function ShopList() {
   return (
     <ul className="flex flex-col gap-12 w-[40.476vw]">
       {shopItems.map((shopItem, index) => (
-        <li key={index} className="w-full flex gap-8">
-          <img src={shopItem.image} alt={shopItem.itemName} />
+        <li key={index} className="w-full flex gap-8 items-center">
+          <img
+            src={shopItem.image}
+            alt={shopItem.itemName}
+            className="h-[210px] w-[280px] object-cover"
+          />
           <div className="flex flex-col gap-2">
             <div>
-              <p>{shopItem.currentPrice}</p>
-              <h4>{shopItem.itemName}</h4>
+              <p className="text-default-size font-Chillax-regular text-primary uppercase">
+                {shopItem.currentPrice}
+              </p>
+              <h4 className="font-Chillax-medium text-default capitalize">
+                {shopItem.itemName}
+              </h4>
             </div>
-            <p>{shopItem.description}</p>
+            <p className="text-default-size text-muted2 font-Chillax-regular">
+              {shopItem.description}
+            </p>
           </div>
         </li>
       ))}
