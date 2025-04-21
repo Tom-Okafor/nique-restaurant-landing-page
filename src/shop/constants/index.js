97f90a6d82b16a876781ec0bab3ee5c5c2501d8a
Tom Okafor <tmeka008@gmail.com>
2025-04-21 23:13:18 +0100

    chore: create shop items data

---
+++ src/shop/constants/index.js
@@ -0,0 +1,30 @@
+export const shopItems = [
+  {
+    itemName: "Gift Card Light",
+    currentPrice: "$15 USD",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit eo ut vitae neque sed sed sit sagittis tristique scelerisque.",
+    image: "/shop/light.png",
+  },
+  {
+    itemName: "Gift Card standard",
+    currentPrice: "$30 USD",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit eo ut vitae neque sed sed sit sagittis tristique scelerisque.",
+    image: "/shop/standard.png",
+  },
+  {
+    itemName: "gift card premium",
+    currentPrice: "$50 USD",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit eo ut vitae neque sed sed sit sagittis tristique scelerisque.",
+    image: "/shop/premium.png",
+  },
+  {
+    itemName: "gift card gold",
+    currentPrice: "$100 USD",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit eo ut vitae neque sed sed sit sagittis tristique scelerisque.",
+    image: "/shop/gold.png",
+  },
+];
